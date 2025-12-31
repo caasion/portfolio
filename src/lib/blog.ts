1942dee0f60eb2d4ea2cad76fa6ab2efe8dc1259
caasion <103465188+caasion@users.noreply.github.com>
2025-12-31 16:06:39 -0500

    Implement blog index parser

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -24,7 +24,7 @@ export async function getPostData(id: string): Promise<Post> {
     const fullPath = path.join(blogDirectory, `${id}.md`);
     const fileContents = fs.readFileSync(fullPath, `utf8`);
 
-    // Parse metadata & destructure markdown file
+    // Use gray-matter to parse metadata & destructure markdown file
     const { data, content } = matter(fileContents);
 
     // Use remark to convert markdown into HTL string
@@ -35,4 +35,35 @@ export async function getPostData(id: string): Promise<Post> {
     const contentHTML = processedContent.toString();
 
     return { id, frontmatter: data as PostData, contentHTML };
+}
+
+/** Gets a list of specific frontmatter data for all posts sorted by date. Used for blog index. */
+export function getSortedBlogData(): PostIndexData[] {
+    // Get file names under /content/blog
+    const fileNames = fs.readdirSync(blogDirectory);
+
+    const allBlogData = fileNames.map((filename) => {
+        // Strip '.md' from filenames to get id
+        const id = filename.replace(/\.md$/, '');
+
+        // Read markdown files as strings
+        const fullPath = path.join(blogDirectory, filename);
+        const fileContents = fs.readFileSync(fullPath, `utf8`);
+
+        // Use gray-matter to parse the post metadata
+        const { data } = matter(fileContents);
+
+        return {
+            id,
+            ...(data as { date: string; title: string })
+        };
+    });
+
+    return allBlogData.sort((a, b) => {
+        if (a.date < b.date) {
+            return 1;
+        } else {
+            return -1;
+        }
+    })
 }
