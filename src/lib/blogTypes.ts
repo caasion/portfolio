141f2533382acde43e92551559565a92dfa0517f
caasion <103465188+caasion@users.noreply.github.com>
2026-01-02 13:54:49 -0500

    Define types for authors and tags

--- src/lib/blogTypes.ts
+++ src/lib/blogTypes.ts
@@ -5,6 +5,8 @@ export interface PostIndexData {
     title: string;
     description: string;
     date: string;
+    authors: string[];
+    tags: string[];
     coverImage: string | null; // Blank if no cover. Otherwise a string of the filename of the cover image (must be in the same directory).
 }
 
