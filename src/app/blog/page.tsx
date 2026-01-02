ce330d38822ef3306fbab74ded50be7f0b91a716
caasion <103465188+caasion@users.noreply.github.com>
2026-01-02 13:55:02 -0500

    Implement tags processing and rendering

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,7 +10,7 @@ export default function BlogIndex() {
       <h1 className="text-3xl font-bold mb-8">My Blog</h1>
       
       <div className="grid gap-6">
-        {allPosts.map(({ id, title, date, description, coverImage }) => (
+        {allPosts.map(({ id, title, date, description, tags, coverImage }) => (
           <Link 
             href={`/blog/${id}`} 
             key={id}
@@ -18,9 +18,15 @@ export default function BlogIndex() {
           >
             <div className="flex">
               <div className=''>
+                <p className="text-gray-500 text-sm mb-2">{date}</p>
                 <h2 className="text-xl font-semibold mb-2">{title}</h2>
-              <p className="text-gray-500 text-sm mb-2">{date}</p>
-              <p className="text-gray-200 text-md">{description}</p>
+                <div className="flex gap-2">
+                  {tags.map(tag => (
+                    <p key={tag} className="text-purple-200 text-sm mb-2">{tag}</p>
+                  ))}
+                </div>
+                
+                <p className="text-gray-200 text-md">{description}</p>
               </div>
               
               <div className='grow-0'>
