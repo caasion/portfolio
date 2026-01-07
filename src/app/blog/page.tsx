e6a840bf4d3af02961e833f5e292691d89601582
caasion <103465188+caasion@users.noreply.github.com>
2026-01-06 21:32:34 -0500

    Centralize layout in root layout

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,15 @@
 import { getSortedBlogData } from '@/lib/blog';
 import BlogList from './_components/BlogList';
 
-export default function BlogIndex() {
 export default function BlogIndexPage() {
     // Get all of the blog posts initially
   const initialPosts = getSortedBlogData();
 
   return (
-    <main className="max-w-3xl mx-auto py-10 px-4">
+    <>
       <h1 className="text-3xl font-bold mb-8">My Blog</h1>
 
       <BlogList initialPosts={initialPosts} />
-    </main>
+    </>
   );
 }
