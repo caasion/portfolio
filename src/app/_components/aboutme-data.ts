27e6d19848af618cdb1a2f234ba4141032963124
caasion <103465188+caasion@users.noreply.github.com>
2026-01-12 09:09:58 -0500

    Refactor about data

---
+++ src/app/_components/aboutme-data.ts
@@ -0,0 +1,9 @@
+export const HEADLINE: string = "Systems Design | Neuroscience";
+export const DESCRIPTION: string = `I like startups, systems, and neuroscience. I build fast, learn faster, and value real connection.`;
+export const CALL_TO_ACTION: string = `Let's get things done.`
+export const BIOGRAPHY: string = 
+`Welcome to my life operating system, BunOS.
+
+I love bunnies, startups, tech, and neuroscience. I love to explore the intersection of brains and machines. I'm obsessed with efficiency, which drove me to building technology that optimizes complex systems in the real world (i.e. the slow airplane boarding process).
+
+I am a Systems Design Engineering student at the University of Waterloo. I execute and learn fast. I learned that as an important trait from building in Waterloo.`;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,5 @@
 import Image from 'next/image';
-
-const HEADLINE: string = "Systems Design | Neuroscience";
-const DESCRIPTION: string = `I like startups, systems, and neuroscience. I build fast, learn faster, and value real connection.
-
-Let's get things done.`;
-const BIOGRAPHY: string = 
-`Welcome to my life operating system, BunOS.
-
-I love bunnies, startups, tech, and neuroscience. I love to explore the intersection of brains and machines. I'm obsessed with efficiency, which drove me to building technology that optimizes complex systems in the real world (i.e. the slow airplane boarding process).
-
-I am a Systems Design Engineering student at the University of Waterloo. I execute and learn fast. I learned that as an important trait from building in Waterloo.`;
+import { HEADLINE, DESCRIPTION, BIOGRAPHY, CALL_TO_ACTION } from '../_components/aboutme-data';
 
 export default function AboutPage() {
   return (
