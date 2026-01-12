e905996bc88066cab9c83a5e65f308c870e364d1
caasion <103465188+caasion@users.noreply.github.com>
2026-01-12 08:42:29 -0500

    Finalize About page
    - Add styling to about page
    - fix image alignment

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ const DESCRIPTION: string = `I like startups, systems, and neuroscience. I build
 
 Let's get things done.`;
 const BIOGRAPHY: string = 
-`Hi! I'm Isaac. Welcome to my life operating system, BunOS.
+`Welcome to my life operating system, BunOS.
 
 I love bunnies, startups, tech, and neuroscience. I love to explore the intersection of brains and machines. I'm obsessed with efficiency, which drove me to building technology that optimizes complex systems in the real world (i.e. the slow airplane boarding process).
 
@@ -17,16 +17,18 @@ export default function AboutPage() {
       <h1>About</h1>
 
       <div className="grid grid-cols-[1fr_2fr] w-full">
-        <div className="relative mx-auto w-4/5">
-          <Image fill src="/images/about/profile.jpg" alt="A profile of Isaac Ng" className="object-cover object-center "></Image>
+        <div className="relative mx-auto my-auto w-4/5 h-[50vh] overflow-hidden">
+          <Image fill src="/images/about/profile.jpg" alt="A profile of Isaac Ng" className="object-cover object-[47%_70%] scale-150"></Image>
         </div>
           
         <div className="max-w-4xl mx-auto">
           <h2 className="text-5xl mb-4">Isaac Ng</h2>
-          <p className="font-mono text-xl mb-12">{HEADLINE}</p>
-          <p className="font-display text-2xl" dangerouslySetInnerHTML={{ __html: DESCRIPTION.replace(/\n/g, '<br />') }}></p>
+          <p className="font-mono text-xl mb-8 text-gray-300">{HEADLINE}</p>
+          <p className="font-display text-2xl mb-6" dangerouslySetInnerHTML={{ __html: DESCRIPTION.replace(/\n/g, '<br />') }}></p>
 
-          <p className="" dangerouslySetInnerHTML={{ __html: BIOGRAPHY.replace(/\n/g, '<br />') }}></p>
+          <hr className="mb-4"></hr>
+
+          <p className="text-gray-300" dangerouslySetInnerHTML={{ __html: BIOGRAPHY.replace(/\n/g, '<br />') }}></p>
         </div>
       </div>
     </>
