0956b9744c14a9a2f8bb106b507f68a9b432866d
caasion <103465188+caasion@users.noreply.github.com>
2026-01-12 09:10:23 -0500

    Create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,31 @@
 import Image from "next/image";
+import { HEADLINE, DESCRIPTION, BIOGRAPHY, CALL_TO_ACTION } from "./_components/aboutme-data";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <div className="grid grid-cols-[2fr_1fr] w-full">
+        <div className="max-w-4xl mx-auto">
+          <h2 className="text-8xl mb-4">Isaac Ng</h2>
+          <p className="font-mono text-xl mb-16 text-gray-300">{HEADLINE}</p>
+          <p className="font-display text-3xl mb-6">{DESCRIPTION}</p>
+          <p className="font-display text-3xl mb-12 text-accent">{CALL_TO_ACTION}</p>
+
+          <div className="flex gap-2">
+            <Link href="/projects">
+              <button className="bg-gray-900 rounded-2xl p-4 text-xl cursor-pointer font-mono">Explore Projects</button>
+            </Link>
+            <Link href="https://www.linkedin.com/in/isaacng0/">
+              <button className="bg-purple-900 rounded-2xl p-4 text-xl cursor-pointer font-mono">Connect</button>
+            </Link>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="relative mx-auto my-auto w-4/5 h-[50vh] overflow-hidden">
+          <Image fill src="/images/about/profile.jpg" alt="A profile of Isaac Ng" className="object-cover object-[47%_70%] scale-150"></Image>
         </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
