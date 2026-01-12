ff3c11f23ffcd48df8c73bae2dff8ed84bc1ce46
caasion <103465188+caasion@users.noreply.github.com>
2026-01-12 10:10:48 -0500

    Add tags to projects

--- src/app/projects/_components/ProjectCard.tsx
+++ src/app/projects/_components/ProjectCard.tsx
@@ -4,15 +4,22 @@ export interface ProjectCardProps {
   title: string;
   subtitle: string;
   description: string;
+  tags: ProjectTag[];
   mediaSrc: string;
   mediaType?: 'image' | 'video';
   mediaPosition?: 'left' | 'right';
+}
+
+export interface ProjectTag {
+  label: string,
+  color: string,
 }
 
 export default function ProjectCard({ 
   title, 
   subtitle, 
   description, 
+  tags,
   mediaSrc, 
   mediaType = 'image',
   mediaPosition = 'right' 
@@ -21,6 +28,12 @@ export default function ProjectCard({
     <div className="flex flex-col lg:flex-row gap-8 items-center mb-24 scroll-mt-20">
       {/* Content Section */}
       <div className={`flex-1 space-y-4 ${mediaPosition === 'left' ? 'lg:order-2' : ''}`}>
+        <div className="flex gap-2">
+          {tags.map(({label, color}, index) => (
+            <div key={index} className={`${colorClasses[color as keyof typeof colorClasses]} border-2 p-2 w-fit font-mono`}>{label}</div>
+          ))}
+          
+        </div>
         <h2 className="text-4xl font-bold">{title}</h2>
         <h3 className="text-xl text-gray-600 dark:text-gray-400">{subtitle}</h3>
         <p className="text-lg leading-relaxed text-gray-700 dark:text-gray-300">
